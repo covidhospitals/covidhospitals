c057fa1f454b32da67bb2acf5fcd4f10bc75930f
Prasanna Kumar Gollangi <prassu158@gmail.com>
2021-05-06 19:28:46 +0530

    Add Delhi data collector proxy function

---
+++ functions/delhi-data-collector/delhi-data-collector.js
@@ -0,0 +1,11 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = async (event) => {
+  try {
+    var response = await fetch(`https://coronabeds.jantasamvad.org/${event.path}`).then(r => r.text());
+    return { statusCode: 500, body: response };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
