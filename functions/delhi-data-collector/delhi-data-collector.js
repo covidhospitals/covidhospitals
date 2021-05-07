f73572c86eda803c5ad3a6b53367788906c3df7d
Prasanna Kumar Gollangi <prassu158@gmail.com>
2021-05-07 20:22:27 +0530

    Fix status code issue

--- functions/delhi-data-collector/delhi-data-collector.js
+++ functions/delhi-data-collector/delhi-data-collector.js
@@ -22,9 +22,9 @@ const handler = async (event) => {
   try {
     var path = event.queryStringParameters.path
     var response = await fetch(`https://coronabeds.jantasamvad.org/${path}`).then(r => r.text());
-    return { statusCode: 500, body: response };
+    return { statusCode: 200, body: response };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 200, body: error.toString() }
   }
 }
 
