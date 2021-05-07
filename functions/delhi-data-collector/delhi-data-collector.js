6f97bfb8ac031c68b68cef83d89f74e36e98e7c1
Prasanna Kumar Gollangi <prassu158@gmail.com>
2021-05-07 13:00:18 +0530

    Fix delhi data collector function

--- functions/delhi-data-collector/delhi-data-collector.js
+++ functions/delhi-data-collector/delhi-data-collector.js
@@ -3,7 +3,8 @@ const fetch = require("node-fetch");
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
   try {
-    var response = await fetch(`https://coronabeds.jantasamvad.org/${event.path}`).then(r => r.text());
+    var path = event.queryStringParameters.path
+    var response = await fetch(`https://coronabeds.jantasamvad.org/${path}`).then(r => r.text());
     return { statusCode: 500, body: response };
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
