1177fe3e945110605fc46401f2be776e00a825d3
Prasanna Kumar Gollangi <prassu158@gmail.com>
2021-05-07 20:17:59 +0530

    Add CORS support

--- functions/delhi-data-collector/delhi-data-collector.js
+++ functions/delhi-data-collector/delhi-data-collector.js
@@ -1,7 +1,24 @@
 const fetch = require("node-fetch");
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
+
+  if (event.httpMethod === 'OPTIONS') {
+    // To enable CORS
+    return {
+      statusCode: 200, // <-- Important!
+      headers,
+      body: 'This was OPTIONS request!'
+    };
+  }
+
+
   try {
     var path = event.queryStringParameters.path
     var response = await fetch(`https://coronabeds.jantasamvad.org/${path}`).then(r => r.text());
