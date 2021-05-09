841efd088a9c91fee0bc8d2dab5990bc3d6e9f9e
Prasanna Kumar Gollangi <prassu158@gmail.com>
2021-05-09 17:43:16 +0530

    Add share capability

--- src/Map.js
+++ src/Map.js
@@ -12,7 +12,7 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 import 'font-awesome/css/font-awesome.min.css';
 
-import 'react-leaflet-markercluster/dist/styles.min.css'; 
+import 'react-leaflet-markercluster/dist/styles.min.css';
 
 import LocateControl from './components/LocateControl'
 import SearchControl from './components/SearchControl'
@@ -59,6 +59,25 @@ class CovidMap extends React.Component {
         map.zoomControl.setPosition('bottomright');
     }
 
+    onHospitalClick(hospital) {
+        console.log(hospital);
+        if (navigator.share) {
+            const shareData = {
+                title: `${hospital.name}`,
+                text: 
+`Phone: ${hospital.phoneNumber}.
+Address: ${hospital.location.formattedAddress}.
+Beds available:- General: ${hospital.general.available}, ICU: ${hospital.icu.available}, O2: ${hospital.o2.available}.
+Get directions: ${hospital.location.link}`,
+                url: "http://covidhospitals.online/",
+            }
+            navigator.share(shareData).then(() => {
+                console.log('Thanks for sharing!');
+            })
+                .catch(console.error);
+        }
+    }
+
     render() {
         const { center } = this.state;
         const locateOptions = {
@@ -81,7 +100,7 @@ class CovidMap extends React.Component {
             <TileLayer
                 attribution='Created by <a href="https://github.com/pgollangi" target="_blank" >github.com/pgollangi</a>'
                 url="https://github.com/pgollangi"
-            /> 
+            />
             <LocateControl key="locate" options={locateOptions} />
             <SearchControl />
             <RightControls dataSources={allHospitals} />
@@ -90,37 +109,35 @@ class CovidMap extends React.Component {
 
                 {allHospitals.map((d) => {
                     return d.hospitals.map((h, idx) => {
+                        h.location.link = h.location.link || `https://www.google.com/maps/search/?api=1&query=${h.location.latitude},${h.location.longitude}`
                         return (
                             <Marker key={`h${idx}`} position={[h.location.latitude, h.location.longitude]} >
                                 <Popup>
                                     <table>
                                         <thead>
                                             <tr>
-                                                <th>Hospital:</th>
-                                                <td colSpan="3">{h.name}</td>
+                                                <th colSpan="2">Hospital:</th>
+                                                <td colSpan="3">{h.name} <i className="fa fa-share-alt h-share" onClick={() => this.onHospitalClick(h)}></i></td>
                                             </tr>
                                             <tr>
-                                                <th>Phone:</th>
+                                                <th colSpan="2">Phone:</th>
                                                 <td colSpan="3">
-                                                    <a href={`tel:${Array.isArray(h.phoneNumber) ? h.phoneNumber[0] : h.phoneNumber}`} target="_blank" rel="noreferrer">
+                                                    <i className="fa fa-phone"></i>
+                                                    <a className="h-value" href={`tel:${Array.isArray(h.phoneNumber) ? h.phoneNumber[0] : h.phoneNumber}`} target="_blank" rel="noreferrer">
                                                         {Array.isArray(h.phoneNumber) ? h.phoneNumber[0] : h.phoneNumber}
                                                     </a>
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <th>Address:</th>
-                                                <td colSpan="3">{h.location.formattedAddress || (h.location.streetName + "," + h.location.city)}</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Directions:</th>
-                                                <td colSpan="3">
-                                                    <a href={h.location.link || `https://www.google.com/maps/search/?api=1&query=${h.location.latitude},${h.location.longitude}`} target="_blank" rel="noreferrer">
-                                                        Click Here
+                                                <th colSpan="2">Address:</th>
+                                                <td colSpan="3"><i className="fa fa-map-marker"></i><span className="h-value">{h.location.formattedAddress || (h.location.streetName + "," + h.location.city)}</span>
+                                                    <a href={h.location.link} target="_blank" rel="noreferrer">
+                                                        (Get Directions)
                                             </a>
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <th>LastUpdated:</th>
+                                                <th colSpan="2">LastUpdated:</th>
                                                 <td colSpan="3">
                                                     {
                                                         ((!h.lastUpdatedAt && !d.lastUpdatedAt) ? ("N/A") : (<ReactTimeAgo date={new Date(h.lastUpdatedAt || d.lastUpdatedAt)} />))
@@ -129,7 +146,7 @@ class CovidMap extends React.Component {
                                             </tr>
                                             <tr>
 
-                                                <th>Beds</th>
+                                                <th colSpan="2">Beds</th>
                                                 <th>Total</th>
                                                 <th>Occupied</th>
                                                 <th>Available</th>
@@ -138,19 +155,19 @@ class CovidMap extends React.Component {
                                         <tbody>
 
                                             <tr>
-                                                <td>General</td>
+                                                <td colSpan="2">General</td>
                                                 <td>{h.general.total || '-'}</td>
                                                 <td>{h.general.occupied || '-'}</td>
                                                 <td>{h.general.available || '-'}</td>
                                             </tr>
                                             <tr>
-                                                <td>ICU</td>
+                                                <td colSpan="2">ICU</td>
                                                 <td>{h.icu.total || '-'}</td>
                                                 <td>{h.icu.occupied || '-'}</td>
                                                 <td>{h.icu.available || '-'}</td>
                                             </tr>
                                             <tr>
-                                                <td>O2</td>
+                                                <td colSpan="2">O2</td>
                                                 <td>{h.o2.total || '-'}</td>
                                                 <td>{h.o2.occupied || '-'}</td>
                                                 <td>{h.o2.available || '-'}</td>
